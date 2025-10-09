dd6b63fa3f69dffd3a3bf21d4ba34e3f82041b8f
Yerraguntla-Krupa-Komala <yerraguntla.krupa-komala@capgemini.com>
2025-10-09 15:06:36 +0530

    committed Category.DTO

--- src/main/java/com/ims/inventorymanagementsystem/service/CategoryService.java
+++ src/main/java/com/ims/inventorymanagementsystem/service/CategoryService.java
@@ -1,4 +1,24 @@
 package com.ims.inventorymanagementsystem.service;
 
+import com.ims.inventorymanagementsystem.dtos.CategoryDTO;
+import com.ims.inventorymanagementsystem.entities.Category;
+import com.ims.inventorymanagementsystem.repositories.CategoryRepo;
+import jakarta.xml.ws.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private CategoryRepo categoryRepo;
+
+    private ModelMapper modelMapper;
+
+    public <Category>Response createCategory(CategoryDTO categoryDTO) {
+        Category category = modelMapper.map(CategoryDTO,Category.class);
+        category.savedCategory = CategoryRepo.save(categoryTosave);
+        return Response.builder()
+                .build()
+    }
 }
