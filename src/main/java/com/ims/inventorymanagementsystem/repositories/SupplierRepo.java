93f7a80777a8dd2a5b968337e2a651b469e467a5
Kalki <kalki.j@capgemini.com>
2025-10-09 12:29:39 +0530

    Committed SupplierRepo.java

--- src/main/java/com/ims/inventorymanagementsystem/repositories/SupplierRepo.java
+++ src/main/java/com/ims/inventorymanagementsystem/repositories/SupplierRepo.java
@@ -1,4 +1,8 @@
 package com.ims.inventorymanagementsystem.repositories;
 
-public interface SupplierRepo {
+import com.ims.inventorymanagementsystem.entities.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SupplierRepo extends JpaRepository<Supplier,Long> {
 }
