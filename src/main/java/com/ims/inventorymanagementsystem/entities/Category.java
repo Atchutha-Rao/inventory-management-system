a13041a51575be84e8a0e16abf4c8aa72fd4f977
Yerraguntla-Krupa-Komala <yerraguntla.krupa-komala@capgemini.com>
2025-10-09 12:28:14 +0530

    committed Category.java

--- src/main/java/com/ims/inventorymanagementsystem/entities/Category.java
+++ src/main/java/com/ims/inventorymanagementsystem/entities/Category.java
@@ -1,4 +1,31 @@
 package com.ims.inventorymanagementsystem.entities;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+@Data
+@Builder
+
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENYITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Transaction> transactions;
 }
