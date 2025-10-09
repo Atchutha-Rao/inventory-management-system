57c2fe8a1eddc3bb682a42b2c3ecde4e0061aa34
Atchutha <atchutha.rao-t.a@gmail.com>
2025-10-09 15:23:12 +0530

    Added Respone wrapper

--- src/main/java/com/ims/inventorymanagementsystem/repositories/CategoryRepo.java
+++ src/main/java/com/ims/inventorymanagementsystem/repositories/CategoryRepo.java
@@ -1,8 +1,11 @@
 package com.ims.inventorymanagementsystem.repositories;
 
+import com.ims.inventorymanagementsystem.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface CategoryRepo {
+public interface CategoryRepo extends JpaRepository<Category,Integer> {
 
-    Optional<Object> findById(Integer categoryId);
+    Optional<Category> findById(Integer categoryId);
 }
