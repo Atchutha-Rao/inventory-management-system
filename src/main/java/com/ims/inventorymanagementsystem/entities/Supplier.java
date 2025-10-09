cac942f20caa7a224862900c8c5a1d1f76ab966e
Kalki <kalki.j@capgemini.com>
2025-10-09 11:10:33 +0530

    Edit Supplier.java

--- src/main/java/com/ims/inventorymanagementsystem/entities/Supplier.java
+++ src/main/java/com/ims/inventorymanagementsystem/entities/Supplier.java
@@ -1,5 +1,29 @@
 package com.ims.inventorymanagementsystem.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Suppliers")
+@Data
+@Builder
 public class Supplier {
-    private int test;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long supplierId;
+    @NotBlank(message = "Name is required")
+    private String supplierName;
+    @NotBlank(message = " supplierContactInfo is required")
+    private String supplierContactInfo;
+    @NotBlank(message = "supplierAddress is required")
+    private String supplierAddress;
+
+
+
 }
