42edcf810397ae30f0b91a9fcc75e87b5c8b81d5
skmuskan <sk-tarannum.muskan@capgemini.com>
2025-10-09 12:22:10 +0530

    Product_Code

--- src/main/java/com/ims/inventorymanagementsystem/entities/Product.java
+++ src/main/java/com/ims/inventorymanagementsystem/entities/Product.java
@@ -1,4 +1,26 @@
 package com.ims.inventorymanagementsystem.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
