f167c9e91d56abcbbcf258f066c003642c3dd54b
Atchutha <atchutha.rao-t.a@gmail.com>
2025-10-09 10:02:21 +0530

    Added User entity

---
+++ src/main/java/com/ims/inventorymanagementsystem/User.java
@@ -0,0 +1,23 @@
+package com.ims.inventorymanagementsystem;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private long phoneNumber;
+}
