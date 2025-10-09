18aba5c2715ca05c0831a10d46d8dbc0429dd7f4
Yerraguntla-Krupa-Komala <yerraguntla.krupa-komala@capgemini.com>
2025-10-09 15:16:40 +0530

    committed CategoryDto

---
+++ src/main/java/com/ims/inventorymanagementsystem/dtos/CategoryDTO.java
@@ -0,0 +1,29 @@
+package com.ims.inventorymanagementsystem.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ims.inventorymanagementsystem.entities.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CategoryDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+  @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+  private List<ProductDTO> products;
+}
