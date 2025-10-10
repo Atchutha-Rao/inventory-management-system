471ac0fd5472f7b1e40a8b205e0391bc94d48aee
Atchutha <atchutha.rao-t.a@gmail.com>
2025-10-10 10:29:47 +0530

    Changes to  AuthFilter and .

--- src/main/java/com/ims/inventorymanagementsystem/security/SecurityConfig.java
+++ src/main/java/com/ims/inventorymanagementsystem/security/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.ims.inventorymanagementsystem.security;
 
-import com.phegondev.InventoryMgtSystem.exceptions.CustomAccessDenialHandler;
-import com.phegondev.InventoryMgtSystem.exceptions.CustomAuthenticationEntryPoint;
+import com.ims.inventorymanagementsystem.exceptions.CustomAccessDenialHandler;
+import com.ims.inventorymanagementsystem.exceptions.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
