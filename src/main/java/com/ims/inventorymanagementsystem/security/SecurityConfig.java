ea9b7fb9d96e9cbac56d1798e045f926358c2ac5
Atchutha <atchutha.rao-t.a@gmail.com>
2025-10-10 10:32:11 +0530

    .

--- src/main/java/com/ims/inventorymanagementsystem/security/SecurityConfig.java
+++ src/main/java/com/ims/inventorymanagementsystem/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ims.inventorymanagementsystem.exceptions.CustomAuthenticationEntryPoi
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
